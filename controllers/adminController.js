1b318729455035be1613d64f8d6d953b29ddefe1
Yohanes Velly <yohanesvelly23@gmail.com>
2023-06-22 11:59:00 +0700

    add dynamic alert and error handling

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,45 +2,88 @@ const Kategori = require('../models/Kategori');
 
 module.exports = {
   viewDashboard: (req, res) => {
-    res.render('admin/dashboard/view_dashboard');
+    res.render('admin/dashboard/view_dashboard', {
+      title: 'Kurator Berita Admin | Dashboard',
+    });
   },
 
   viewKategori: async (req, res) => {
-    const kategori = await Kategori.find();
-    // console.log(kategori);
-    res.render('admin/kategori/view_kategori', { kategori });
+    try {
+      const kategori = await Kategori.find();
+      // console.log(kategori);
+      const alertMessage = req.flash('alertMessage');
+      const alertStatus = req.flash('alertStatus');
+      const alert = { message: alertMessage, status: alertStatus };
+      res.render('admin/kategori/view_kategori', {
+        kategori,
+        alert,
+        title: 'Kurator Berita Admin | Kategori',
+      });
+    } catch (error) {
+      res.redirect('/admin/kategori');
+    }
   },
   addKategori: async (req, res) => {
-    const { jenis } = req.body;
-    // console.log(jenis);
-    await Kategori.create({ jenis });
-    res.redirect('/admin/kategori');
+    try {
+      const { jenis } = req.body;
+      // console.log(jenis);
+      await Kategori.create({ jenis });
+      req.flash('alertMessage', 'Berhasil Menambahkan Kategori Baru');
+      req.flash('alertStatus', 'success');
+      res.redirect('/admin/kategori');
+    } catch (error) {
+      req.flash('alertMessage', `$error.message`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/admin/kategori');
+    }
   },
   editKategori: async (req, res) => {
-    const { id, jenis } = req.body;
-    const kategori = await Kategori.findOne({ _id: id });
-    // console.log(kategori);
-    kategori.jenis = jenis;
-    await kategori.save();
-    res.redirect('/admin/kategori');
+    try {
+      const { id, jenis } = req.body;
+      const kategori = await Kategori.findOne({ _id: id });
+      // console.log(kategori);
+      kategori.jenis = jenis;
+      await kategori.save();
+      req.flash('alertMessage', 'Berhasil Merubah Kategori');
+      req.flash('alertStatus', 'success');
+      res.redirect('/admin/kategori');
+    } catch (error) {
+      req.flash('alertMessage', `$error.message`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/admin/kategori');
+    }
   },
   deleteKategori: async (req, res) => {
-    const { id } = req.params;
-    await Kategori.findOneAndDelete({ _id: id });
-    // console.log(kategori);
-    // await kategori.remove();
-    res.redirect('/admin/kategori');
+    try {
+      const { id } = req.params;
+      await Kategori.findOneAndDelete({ _id: id });
+      // console.log(kategori);
+      // await kategori.remove();
+      req.flash('alertMessage', 'Berhasil Menghapus Kategori');
+      req.flash('alertStatus', 'success');
+      res.redirect('/admin/kategori');
+    } catch (error) {
+      req.flash('alertMessage', `$error.message`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/admin/kategori');
+    }
   },
 
   viewUsers: (req, res) => {
-    res.render('admin/users/view_users');
+    res.render('admin/users/view_users', {
+      title: 'Kurator Berita Admin | Users',
+    });
   },
 
   viewBerita: (req, res) => {
-    res.render('admin/berita/view_berita');
+    res.render('admin/berita/view_berita', {
+      title: 'Kurator Berita Admin | Berita',
+    });
   },
 
   viewPencarian: (req, res) => {
-    res.render('admin/pencarian/view_pencarian');
+    res.render('admin/pencarian/view_pencarian', {
+      title: 'Kurator Berita Admin | Pencarian',
+    });
   },
 };
